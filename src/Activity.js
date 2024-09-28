d2494d4216745631f65eed81a7e9652aa30d4f10
rashirt <prince_palad@rcc.edu.ph>
2024-09-28 11:04:12 +0800

    num1

---
+++ src/Activity.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Activity = () => {
+  const message = "Hello Everyone!";
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default Activity;
